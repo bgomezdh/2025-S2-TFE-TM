cae9e5095ffa3f93deeb06e6861d1948f5bfa712
bgomezdh <bgomez@digitalhouse.com>
2025-09-19 12:37:11 -0300

    clase de JS

---
+++ clase6-js/demo/js/arrays.js
@@ -0,0 +1,28 @@
+/* let paises = ["Argentina", "Brasil", "Chile", "Venezuela"];
+
+console.log("Yo vivo en el pais llamado: " + paises[20]);
+
+
+console.log("Este array tiene " + paises.length + " paises.");
+
+let ultimoPais = paises.pop();
+
+console.log("El pais eliminado es: " + ultimoPais);
+
+
+paises.push("Ecuador");
+
+console.log(paises); */
+
+
+let arrayDeArrays = [ ["Abuelo Paterno", "Abuela Paterna"] , ["Abuelo Materno", "Abuela Materna"] ];
+
+let nombreAbuelaPaterna = arrayDeArrays[0][1];
+
+console.log(nombreAbuelaPaterna + " se llama Maria;");
+
+
+console.log(arrayDeArrays[0]);
+
+console.log(arrayDeArrays[1]);
+
---
+++ clase6-js/demo/js/index.js
@@ -0,0 +1,41 @@
+console.log("Probando la terminal desde js");
+
+console.log("Saludos segundo mensaje: " + 44);
+
+// esto es un comentario
+
+let nombrePersonaje = "El barto";
+
+nombrePersonaje = "Lisa";
+
+
+
+
+/*
+Tipos de datos
+*/ 
+
+let nombre = "Brian"; // String
+
+let edad = 31; // int or decimal
+
+let esMayorDeEdad = true; // Booleans
+
+let div = "hola maestro " / 45; // Not a Number
+
+let noExiste = null;
+
+console.log(noExiste);
+
+
+noExiste = "hola"
+
+let indefinido; // undefined
+
+indefinido = 44;
+
+console.log(indefinido);
+
+
+
+
